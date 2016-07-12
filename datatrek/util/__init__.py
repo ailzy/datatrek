b3ee79ec6daed7a69027a7b65c554782329be77a
Qiang Luo <hzluoqiang@corp.netease.com>
2016-07-12 14:12:32 +0800

    add with_cache and DataFrameNDArrayWrapper

--- datatrek/util/__init__.py
+++ datatrek/util/__init__.py
@@ -28,3 +28,33 @@ def df_to_records(df):
 def df_to_clean_records(df):
     return list(map(remove_null_values_in_dict, df_to_records(df)))
 
+import os
+import pickle
+def with_cache(cache_file):
+    def f(g):
+        if os.path.exists(cache_file):
+            return pickle.load(open(cache_file, 'rb'))
+        else:
+            res = g()
+            pickle.dump(res, open(cache_file, 'wb'),
+                        protocol=pickle.HIGHEST_PROTOCOL)
+            return res
+    return f
+
+
+from collections import OrderedDict
+def list_to_location_map(l):
+    return dict(zip(l, range(len(l))))
+class DataFrameNDArrayWrapper:
+
+    def __init__(self, df):
+        self.d = df.values
+        self.row_names = df.index.tolist()
+        self.column_names = df.columns.tolist()
+        self.row_name_to_loc = list_to_location_map(self.row_names)
+        self.column_name_to_loc = list_to_location_map(self.column_names)
+
+    def get_row_as_dict(self, row_name):
+        i = self.row_name_to_loc[row_name]
+        row = self.d[i]
+        return OrderedDict(zip(self.column_names, row))
