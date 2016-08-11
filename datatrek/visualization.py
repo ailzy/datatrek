3699d974be814f7436beab446d677cdf280f2e80
Qiang Luo <hzluoqiang@corp.netease.com>
2016-08-11 10:03:20 +0800

    plot corr cluster

--- datatrek/visualization.py
+++ datatrek/visualization.py
@@ -48,3 +48,72 @@ def visualize_distribution(x):
         axes[1,i].hist(x_new)
         axes[0,i].set_title(name+', coverage {:.2g}'.format(coverage))
     return fig
+
+from scipy.stats.mstats import mquantiles
+import fastcluster as fst
+import seaborn as sns
+from scipy.cluster import hierarchy
+import matplotlib.patches as patches
+def plot_corr_cluster(m, method=1, **kargs):
+    sns.set_style('white')
+
+    l = fst.linkage(m, method='average')
+
+    if method == 1:
+        # Threshold 1: MATLAB-like behavior
+        t = 0.7*max(l[:, 2])
+    elif method == 2:
+        t = np.median(hierarchy.maxdists(l))
+    elif method == 3:
+        t= mquantiles(hierarchy.maxdists(l), prob=0.75)[0]
+    else:
+        raise RuntimeError('no such method')
+
+    # Plot the clustermap
+    # Save the returned object for further plotting
+    mclust = sns.clustermap(m,
+                            linewidths=0,
+                            cmap=plt.get_cmap('RdBu'),
+                            vmax=1,
+                            vmin=-1,
+                            row_linkage=l,
+                            col_linkage=l,
+                            **kargs)
+
+    # Draw the threshold lines
+    mclust.ax_col_dendrogram.hlines(t,
+                                    0,
+                                    m.shape[0]*10,
+                                    colors='g',
+                                    linewidths=2,
+                                    zorder=1)
+    mclust.ax_row_dendrogram.vlines(t,
+                                    0,
+                                    m.shape[0]*10,
+                                    colors='g',
+                                    linewidths=2,
+                                    zorder=1)
+
+    # Extract the clusters
+    clusters = hierarchy.fcluster(l, t, 'distance')
+    for c in set(clusters):
+        # Retrieve the position in the clustered matrix
+        index = [x for x in range(m.shape[0])
+                 if mclust.data2d.columns[x] in m.index[clusters == c]]
+        # No singletons, please
+        if len(index) == 1:
+            continue
+
+        # Draw a rectangle around the cluster
+        mclust.ax_heatmap.add_patch(
+            patches.Rectangle(
+                (min(index),
+                 m.shape[0] - max(index) - 1),
+                len(index),
+                len(index),
+                facecolor='none',
+                edgecolor='g',
+                lw=3)
+        )
+
+    plt.title('Cluster matrix')
